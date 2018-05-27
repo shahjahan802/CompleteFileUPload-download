0c4d9040e76e520a6631ab10f13d223d66457d63
shahjahan <shahjahan7868@outlook.com>
2018-05-27 16:46:58 -0700

    Add project files.

---
+++ FileUpload26MayTest/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FileUpload26MayTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ FileUpload26MayTest/Models/ManageViewModels/ShowRecoveryCodesViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FileUpload26MayTest.Models.ManageViewModels
+{
+    public class ShowRecoveryCodesViewModel
+    {
+        public string[] RecoveryCodes { get; set; }
+    }
+}

654dbcb7a7fcbb76883920dc35a397d5f9002c78
shahjahan <shahjahan7868@outlook.com>
2018-05-30 00:28:52 -0700

    online database with complete flow Media file upload and Download

--- FileUpload26MayTest/Controllers/FirstController.cs
+++ FileUpload26MayTest/Controllers/FirstController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
 using System.IO;
+using MimeSharp;
 
 namespace FileUpload26MayTest.Controllers
 {
@@ -30,31 +31,64 @@ namespace FileUpload26MayTest.Controllers
             return View();
         }
         [HttpPost]
-        public IActionResult FileMediaCreate(FormTable f , IFormFile FilePath)
+        public IActionResult FileMediaCreate(FormTable f, IList<IFormFile> FilePicture, IFormFile FileDownload)
         {
-             if (FilePath != null)
+            foreach (var FilePath in FilePicture)
+            {
+                if (FilePath != null)
                 {
                     string extention = Path.GetExtension(FilePath.FileName);
                     string filenewname = DateTime.Now.ToString("ddmmyyyyhhmmsstt") + "pfn";
                     string finalpath = "/fpicpath/" + filenewname + extention;
-                    FilePath.CopyTo(new FileStream(IHEN.WebRootPath + finalpath, FileMode.Create));
+                    FileStream FN = new FileStream(IHEN.WebRootPath + finalpath, FileMode.Create);
+                    FilePath.CopyTo(FN);
+                    FN.Close();
                     f.FilePath = finalpath;
                     ViewBag.Message = "media file successful save path in online database";
                 }
-      
-                ORM.FormTable.Add(f);
+            }
+
+               if (FileDownload != null)
+                {
+                    string extention = Path.GetExtension(FileDownload.FileName);
+                    string filenewname = DateTime.Now.ToString("ddmmyyyyhhmmsstt") + "dld";
+                    string finalpath ="/fpicpath/" + filenewname + extention;
+                    FileStream FN = new FileStream(IHEN.WebRootPath + finalpath, FileMode.Create);
+                    FileDownload.CopyTo(FN);
+                    FN.Close();
+                    f.FileDownload = finalpath;
+                    ViewBag.Message = "media file successful save path in online database";
+                }
+
+            ORM.FormTable.Add(f);
             ORM.SaveChanges();
             return View();
         }
-
+        [HttpGet]
         public IActionResult FileMediaShow()
         {
-            List<FormTable> ob = ORM.FormTable.ToList<FormTable>();
-           
-            return View(ob);
+            List<FormTable> obj = ORM.FormTable.ToList<FormTable>();  
+            return View(obj);
         }
-       
-    }
+        [HttpGet]
+        public FileResult FileDownload(string Cv)
+        {
+            string  Extention = Path.GetExtension(Cv);
+            Mime Omine = new Mime();
+             string FileType = Omine.Lookup(Extention);
+            return File(Cv, FileType);
+        }
+        [HttpGet]
+        public FileResult FileDownloadD(string Cvv)
+        {
+            string  Extention = Path.GetExtension(Cvv);
+            Mime Omine = new Mime();
+             string FileType = Omine.Lookup(Extention);
+            return File(Cvv, FileType);
+        }
+
+
+}
 
    
 }
--- FileUpload26MayTest/Models/FormTable.cs
+++ FileUpload26MayTest/Models/FormTable.cs
@@ -6,10 +6,12 @@ namespace FileUpload26MayTest.Models
     public partial class FormTable
     {
         public int Id { get; set; }
-        
+        public string Name { get; set; }
         public string Address { get; set; }
         public decimal? PhoneNo { get; set; }
         public string FilePath { get; set; }
+        public string FileDownload { get; set; }
+      
        
     }
 }
